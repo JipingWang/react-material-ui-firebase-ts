4e512551eea11c4f1c2d22dfe09e14d8ab992f14
Jiping <WangJiping@gmail.com>
2019-06-09 22:37:10 -0400

    allow any

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,7 @@ module.exports = {
         
         "@typescript-eslint/indent": "off",
         "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-object-literal-type-assertion": "off",
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-member-accessibility": "off",
